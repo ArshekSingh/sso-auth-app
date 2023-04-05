5db6a453fb32a3c4ed2ee9d2659d0f583ddb2ff4
Arun Mudgal <arun.mudgal@sastechstudio.com>
2023-04-05 15:53:09 +0530

    Crud_implementation

--- src/main/java/com/sas/sso/service/UserService.java
+++ src/main/java/com/sas/sso/service/UserService.java
@@ -2,6 +2,8 @@ package com.sas.sso.service;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.sas.sso.dto.Response;
+import com.sas.sso.dto.UserDto;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sas.sso.dto.LoginDTO;
@@ -11,4 +13,10 @@ public interface UserService {
 	
 	ModelAndView authenticateUser(LoginDTO loginDTO,HttpServletResponse response);
 	ModelAndView redirectAuthenticatedUser(String token,String string, HttpServletResponse response);
+
+	Response createUser(UserDto userDto);
+
+	Response updateUser(UserDto userDto);
+
+	Response findByIdOrNameOrEmail(Long id, String name, String email);
 }
--- src/main/java/com/sas/sso/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/sas/sso/serviceimpl/UserServiceImpl.java
@@ -1,24 +1,22 @@
 package com.sas.sso.serviceimpl;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Optional;
-import java.util.SimpleTimeZone;
+import java.util.*;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.sas.sso.dao.UserDao;
+import com.sas.sso.dto.Response;
+import com.sas.sso.dto.UserDto;
+import com.sas.sso.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sas.sso.dto.LoginDTO;
-import com.sas.sso.entity.AppMaster;
-import com.sas.sso.entity.CompanyMaster;
-import com.sas.sso.entity.TokenSession;
-import com.sas.sso.entity.User;
 import com.sas.sso.repository.AppMasterRepository;
 import com.sas.sso.repository.UserRepository;
 import com.sas.sso.service.UserService;
@@ -49,6 +47,9 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserUtils userUtils;
+
+	@Autowired
+	UserDao userDao;
 
 	@Override
 	public ModelAndView authenticateUser(LoginDTO loginDTO, HttpServletResponse response) {
@@ -151,4 +152,106 @@ public class UserServiceImpl implements UserService {
 		return modelAndView;
 	}
 
+	@Override
+	public Response createUser(UserDto userDto) {
+		log.info("request to create user initiatied successfully");
+		Optional<User> userOptional = userRepository.findByEmail(userDto.getEmail());
+		if(userOptional.isPresent()){
+			log.info("user is already present with email ->{}",userDto.getEmail());
+			return new Response("USER IS ALREADY PRESENT WITH THIS EMIAL ID", HttpStatus.BAD_REQUEST);
+		}
+		User user = new User();
+		user.setEmail(userDto.getEmail());
+		user.setFirstName(userDto.getFirstName());
+		user.setLastname(userDto.getLastname());
+		user.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+		//set company Master
+		CompanyMaster companyMaster = new CompanyMaster();
+		companyMaster.setCompanyName(userDto.getCompanyMaster().getCompanyName());
+		companyMaster.setCompanyName(userDto.getCompanyMaster().getCompanyName());
+		companyMaster.setWebsiteUrl(userDto.getCompanyMaster().getWebsiteUrl());
+		user.setCompanyMaster(companyMaster);
+
+		//set Access group
+		for(AccessGroup accessGroup1 : userDto.getAccessGroups()) {
+			AccessGroup accessGroup = new AccessGroup();
+			accessGroup.setAccessGroupNo(accessGroup1.getAccessGroupNo());
+			accessGroup.setDescription(accessGroup1.getDescription());
+			accessGroup.setName(accessGroup1.getName());
+			accessGroup.setActive(true);
+			accessGroup.setEditable(accessGroup1.getEditable());
+			accessGroup.setCreatedBy(accessGroup1.getCreatedBy());
+			user.setAccessGroups((Set<AccessGroup>) accessGroup);
+		}
+
+		userRepository.save(user);
+		log.info("User saved successfully");
+		return  new Response("USER CREATED SUCCESSFULLY",HttpStatus.OK);
+	}
+
+	@Override
+	public Response updateUser(UserDto userDto) {
+		log.info("Request to update user invoked for emailId -> {}",userDto.getEmail());
+		Optional<User> userOptional = userRepository.findByEmail(userDto.getEmail());
+		if(userOptional.isEmpty()){
+			log.info("User not found for this emailId ->{}",userDto.getEmail());
+			return new Response("User not present for this email id -> {}",userDto.getEmail(),HttpStatus.BAD_REQUEST);
+		}
+		User user = new User();
+		user.setFirstName(userDto.getFirstName());
+		user.setLastname(userDto.getLastname());
+		user.setEmail(userDto.getEmail());
+		user.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+		//set company Master
+		CompanyMaster companyMaster = new CompanyMaster();
+		companyMaster.setCompanyName(userDto.getCompanyMaster().getCompanyName());
+		companyMaster.setCompanyName(userDto.getCompanyMaster().getCompanyName());
+		companyMaster.setWebsiteUrl(userDto.getCompanyMaster().getWebsiteUrl());
+		user.setCompanyMaster(companyMaster);
+
+		//set Access group
+		for(AccessGroup accessGrp : userDto.getAccessGroups()){
+			AccessGroup accessGroup = new AccessGroup();
+			accessGroup.setAccessGroupNo(accessGrp.getAccessGroupNo());
+			accessGroup.setDescription(accessGrp.getDescription());
+			accessGroup.setName(accessGrp.getName());
+			accessGroup.setEditable(accessGrp.getEditable());
+			user.setAccessGroups((Set<AccessGroup>) accessGroup);
+		}
+
+		userRepository.save(user);
+		log.info("User updated successfully for emailId -> {}",userDto.getEmail());
+		return new Response("User updated successfully",HttpStatus.OK);
+	}
+
+	@Override
+	public Response findByIdOrNameOrEmail(Long id, String name, String email) {
+		User userOptional = userDao.findUserByIdOrNameOrEmail(id,name,email);
+		if(userOptional==null){
+			return new Response("User Not Found",HttpStatus.BAD_REQUEST);
+		}
+		User user = new User();
+		user.setId(userOptional.getId());
+		user.setEmail(userOptional.getEmail());
+		user.setFirstName(userOptional.getFirstName());
+		user.setLastname(userOptional.getLastname());
+		//get company Master
+		CompanyMaster companyMaster = new CompanyMaster();
+		companyMaster.setCompanyName(userOptional.getCompanyMaster().getCompanyName());
+		companyMaster.setCompanyName(userOptional.getCompanyMaster().getCompanyName());
+		companyMaster.setWebsiteUrl(userOptional.getCompanyMaster().getWebsiteUrl());
+		user.setCompanyMaster(companyMaster);
+
+		//get Access group
+		for(AccessGroup accessGrp : userOptional.getAccessGroups()){
+			AccessGroup accessGroup = new AccessGroup();
+			accessGroup.setAccessGroupNo(accessGrp.getAccessGroupNo());
+			accessGroup.setDescription(accessGrp.getDescription());
+			accessGroup.setName(accessGrp.getName());
+			accessGroup.setEditable(accessGrp.getEditable());
+			user.setAccessGroups((Set<AccessGroup>) accessGroup);
+		}
+		return new Response("User found successfully",user,HttpStatus.OK);
+
+	}
 }
